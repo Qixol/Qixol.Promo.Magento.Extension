fb6178d3e316693ce828065a0a539d2867b83fd0
Ken Westgate <ken.westgate@qixol.com>
2016-08-16 13:18:19 +0100

    git unlink script updated

--- Scripts/RemoveSymlinks.sh
+++ Scripts/RemoveSymlinks.sh
@@ -1,27 +1,29 @@
 # removes symbolic links from the magento installed directory to the git repo directory
 
+magentoPath="/var/www/magento"
+
 # app directories and files
-unlink /home/ken/public_html/magento-store.com/public/app/etc/modules/Holbi_Qixol.xml
-rm -rf /home/ken/public_html/magento-store.com/public/app/code/community/Holbi
-unlink /home/ken/public_html/magento-store.com/public/app/design/adminhtml/default/default/layout/qixol.xml
-rm -rf /home/ken/public_html/magento-store.com/public/app/design/adminhtml/default/default/template/qixol
-unlink /home/ken/public_html/magento-store.com/public/app/design/frontend/base/default/layout/qixol.xml
-rm -rf /home/ken/public_html/magento-store.com/public/app/design/frontend/base/default/template/qixol
-rm -rf /home/ken/public_html/magento-store.com/public/app/design/frontend/rwd/default/template/qixol
-unlink /home/ken/public_html/magento-store.com/public/app/locale/en_US/Holbi_Qixol.csv
+unlink $magentoPath/app/etc/modules/Holbi_Qixol.xml
+rm -rf $magentoPath/app/code/community/Holbi
+unlink $magentoPath/app/design/adminhtml/default/default/layout/qixol.xml
+rm -rf $magentoPath/app/design/adminhtml/default/default/template/qixol
+unlink $magentoPath/app/design/frontend/base/default/layout/qixol.xml
+rm -rf $magentoPath/app/design/frontend/base/default/template/qixol
+rm -rf $magentoPath/app/design/frontend/rwd/default/template/qixol
+unlink $magentoPath/app/locale/en_US/Holbi_Qixol.csv
 
 # media directories and files
 # TODO: should this directory be renamed to qixol_promo?
-rm -rf /home/ken/public_html/magento-store.com/public/media/custom
+rm -rf $magentoPath/media/custom
 
 # skin directories and files
-unlink /home/ken/public_html/magento-store.com/public/skin/adminhtml/default/default/images/_run.gif
-unlink /home/ken/public_html/magento-store.com/public/skin/adminhtml/default/default/images/_yes.gif
-unlink /home/ken/public_html/magento-store.com/public/skin/frontend/base/default/css/qixol.css
-rm -rf /home/ken/public_html/magento-store.com/public/skin/frontend/base/default/qixol
-rm -rf /home/ken/public_html/magento-store.com/public/skin/frontend/base/default/media
-rm -rf /home/ken/public_html/magento-store.com/public/skin/frontend/base/default/js/lib
+unlink $magentoPath/skin/adminhtml/default/default/images/_run.gif
+unlink $magentoPath/skin/adminhtml/default/default/images/_yes.gif
+unlink $magentoPath/skin/frontend/base/default/css/qixol.css
+rm -rf $magentoPath/skin/frontend/base/default/qixol
+rm -rf $magentoPath/skin/frontend/base/default/media
+rm -rf $magentoPath/skin/frontend/base/default/js/lib
 
 # var directories and files
-rm -rf /home/ken/public_html/magento-store.com/public/var/logs_qixol
+rm -rf $magentoPath/var/logs_qixol
 
