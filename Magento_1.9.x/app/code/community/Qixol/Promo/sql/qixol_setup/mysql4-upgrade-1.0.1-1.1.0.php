1d3bdfaf84248de69d74c06f4c1ebc2382e45ae4
Ken Westgate <ken.westgate@qixol.com>
2016-11-24 10:57:35 +0000

    integraion -> integration (sql update script)

--- Magento_1.9.x/app/code/community/Qixol/Promo/sql/qixol_setup/mysql4-upgrade-1.0.1-1.1.0.php
+++ Magento_1.9.x/app/code/community/Qixol/Promo/sql/qixol_setup/mysql4-upgrade-1.0.1-1.1.0.php
@@ -4,10 +4,9 @@ $installer = $this;
 $installer->startSetup();
 
 $sql=<<<SQLTEXT
-update `core_config_data` set path = replace(path, 'qixol/integraion', 'qixol/integration') WHERE path like 'qixol/integraion%'
-   ALTER TABLE {$installer->getTable('qixol_promotions_type')}
-    CHANGE `from_date` `from_date` DATETIME NOT NULL DEFAULT '0001-01-01 00:00:00',
-    DROP `is_everyday`
+update `core_config_data`
+set path = replace(path, 'qixol/integraion', 'qixol/integration')
+WHERE path like 'qixol/integraion%'
 SQLTEXT;
 $installer->run($sql);
 
